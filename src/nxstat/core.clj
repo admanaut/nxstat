a564db3a91a1d8904406e97f204528933dfe337d
Adrian Marina <adrianmarina.adrian@gmail.com>
2016-05-06 00:41:38 +0100

    load log files into incanter dataset

--- src/nxstat/core.clj
+++ src/nxstat/core.clj
@@ -1,34 +1,105 @@
 (ns nxstat.core
   (:require [clojure.java.io :as io]
             [incanter.core :as incanter]
-            [clojure.string :as string])
+            [incanter.charts :as icharts]
+            [clojure.string :as string]
+            [clj-time.core :as t]
+            [clj-time.format :as f])
   (:gen-class))
 
+(def headers [:remote_addr
+              :time_local
+              :request_method
+              :request_path
+              :status
+              :body_bytes_sent
+              :http_referer
+              :http_user_agent
+              :http_x_forwarded_for
+              :request_time
+              :connection])
 
-(def headers [:GEOID :SUMLEV :STATE :COUNTY :CBSA :CSA :NECTA :CNECTA :NAME :POP100 :HU100 :POP100.2000 :HU100.2000 :P035001 :P035001.2000
-])
+(def line-regex  #"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})? - - \[(.*)\] \"(\w+) ([^\"]*)\"(\d{3}) (\d+) \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\"\[(.*)\] \[(.*)\]")
 
-(defn ls
-  [^String dir]
-  (lazy-seq (sort (.list (io/file dir)))))
+(def date-format "dd/MMM/yyyy:h:m:s Z")
+(def date-formatter (f/formatter date-format))
 
-(defn split-line
+(defn parse-date
+  ([date] (parse-date date date-formatter))
+  ([^String date fmtt]
+   (try
+     (f/parse fmtt date)
+     (catch Exception ex
+       nil))))
+
+(defn parse-line
   [^String line]
-  (string/split line #","))
+  (if-let [matches (re-find line-regex line)]
+    (apply hash-map (interleave headers (next matches)))))
 
-(defn lines-seq
+
+(def file-pattern #"access.log(.*)")
+(def file-matches #(re-matches file-pattern %))
+(def asc compare)
+(def desc #(compare %2 %1))
+
+(defn ls-dir
+  [^String dir ffilter fsort]
+  (sort fsort (filter ffilter (.list (io/file dir) ))))
+
+(defn lazy-read-lines
+  [file]
+  (let [in-file (io/reader file)
+        line-seq (line-seq in-file)
+        lazy (fn lazy [wrapped]
+               (lazy-seq
+                (if-let [s (seq wrapped)]
+                  (cons (first s) (lazy (rest s)))
+                  (.close in-file))))]
+    (lazy line-seq)))
+
+(defn lazy-read-files
   [files]
-  (when-let [f (first files)]
-    (cons (line-seq (io/reader f)) (lazy-seq (lines-seq (next files))))))
+  (let [lazy-lines (fn lazy-lines [lines-seq fs]
+                     (lazy-seq
+                      (cond (seq lines-seq) (cons (first lines-seq) (lazy-lines (rest lines-seq) fs))
+                            (seq fs) (let [l (lazy-read-lines (first fs))]
+                                       (cons (first l) (lazy-lines (rest l) (rest fs))))
+                            :else nil)))]
+    (when-let [fx (seq files)]
+      (lazy-lines (lazy-read-lines (first fx)) (rest fx)))))
 
+(defn load-log-file
+  [file]
+  (->>
+   (map parse-line )
+   (incanter/dataset headers)))
 
 (defn load-logs
-  [dir]
-  (lines-seq (map #(str dir %) (ls dir)) ))
+  [log-files]
+  (->> log-files
+       lazy-read-files
+       (map parse-line)
+       (incanter/dataset headers)))
 
-;(incanter/dataset headers lines)
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+
+
+(comment
+
+  (defn lines-seq
+    [files]
+    (when-let [f (first files)]
+      (cons (line-seq (io/reader f)) (lazy-seq (lines-seq (rest files))))))
+
+  (defn load-logs
+    [log-files]
+    (->> (map )
+         ))
+
+  #(str dir %) (ls-dir dir)
+
+  (defn -main
+    "I don't do a whole lot ... yet."
+    [& args]
+    (println "Hello, World!")))
